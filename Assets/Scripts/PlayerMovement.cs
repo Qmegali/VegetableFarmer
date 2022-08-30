206900ba3db42129fadd5d21ea4b09dda962f29c
Qmegali <jeremyjeong02@gmail.com>
2022-08-30 18:26:22 -0400

    Script Cleanup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,15 +11,16 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private InputActionReference _moveAction;
     [SerializeField] private InputActionReference _jumpAction, _crouchAction;
-    [SerializeField] private InputActionReference _specialAction;
+    [SerializeField] private InputActionReference _specialAction, _restartAction;
+    private float movementInput = 0f;
 
     // ^ Consider replacement for the action reference list? Move inputs to an input manager and subscribe to them from this class? That would require it present in every scene; too much?
 
     [Space, Header("Physics References")]
 
     [SerializeField] private LayerMask _ground;
-    [SerializeField] private BoxCollider2D _boxCollider2D;
-    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private Rigidbody2D rb;
 
     //
 
@@ -31,9 +33,11 @@ public class PlayerMovement : MonoBehaviour
     [Space, Header("Movement Parameters")]
 
     [SerializeField] private float moveSpeed = 8f;
-    [SerializeField] private float moveAccel = 20f, jumpMinHeight = 1.30f, jumpExtraHeight = 3.00f;
+    [SerializeField] private float moveAccel = 20f, airControl = 0.5f, jumpMinHeight = 1.30f, jumpExtraHeight = 3.00f;
+    [SerializeField] private float wallSlideSpeed = 1f;
 
-    private bool _isGrounded = true, _wasGrounded = true, _isCrouched = false;
+    private bool isGrounded = true, wasGrounded = true, isCrouched = false;
+    private float wallSlidingDirection = 0f; 
 
     private UnityAction _LandEvent;
     private IEnumerator _jumpExtensionCoroutine;
@@ -49,6 +53,7 @@ public class PlayerMovement : MonoBehaviour
         _crouchAction.action.started += StartCrouch;
         _crouchAction.action.canceled += CancelCrouch;
         _specialAction.action.performed += CycleSpecial;
+        _restartAction.action.performed += Restart;
 
         //_boxCollider2D = GetComponent<BoxCollider2D>();
         //_rb = GetComponent<Rigidbody2D>();
@@ -61,17 +66,21 @@ public class PlayerMovement : MonoBehaviour
         _crouchAction.action.started -= StartCrouch;
         _crouchAction.action.canceled -= CancelCrouch;
         _specialAction.action.performed -= CycleSpecial;
+        _restartAction.action.performed -= Restart;
     }
 
     void Update()
     {
-        _wasGrounded = _isGrounded;
-        _isGrounded = IsGrounded();
-        if (!_wasGrounded && _isGrounded)
+        movementInput = _moveAction.action.ReadValue<float>();
+
+        wasGrounded = isGrounded;
+        isGrounded = IsGrounded();
+        if (!wasGrounded && isGrounded)
         {
             //Debug.Log("Landed!");
             _LandEvent?.Invoke();
         }
+        wallSlidingDirection = WallSliding(movementInput);
     }
 
     private void LateUpdate()
@@ -86,7 +95,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (_rb.gravityScale == 0 && _rb.velocity.y < 0.2f * Mathf.Sqrt(-2f * jumpMinHeight * Physics2D.gravity.y)) // If you bump your head while jumping
+        if (rb.gravityScale == 0 && rb.velocity.y < 0.2f * Mathf.Sqrt(-2f * jumpMinHeight * Physics2D.gravity.y)) // If you bump your head while jumping
         {
             CancelJump();
 
@@ -102,14 +111,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        float movementInput = _moveAction.action.ReadValue<float>();
+        // Horizontal movement
 
-        float xVelocity = _rb.velocity.x;
+        float xVelocity = rb.velocity.x;
         float xSpeed = Mathf.Abs(xVelocity);
 
-        float xInputDirection = movementInput/Mathf.Abs(movementInput);
-        float xDirection = xVelocity/xSpeed;
-        if (_isCrouched && _isGrounded) { movementInput = 0f; }
+        float xInputDirection = movementInput == 0f ? 0f : Mathf.Sign(movementInput);
+        float xDirection = Mathf.Sign(xVelocity);
+        if (isCrouched && isGrounded) { movementInput = 0f; }
 
         float xSpeedInInputDirection = xInputDirection * xVelocity;
         float xSpeedToAdd = moveAccel * Time.deltaTime;
@@ -119,19 +128,19 @@ public class PlayerMovement : MonoBehaviour
             if (xSpeedInInputDirection <= moveSpeed) // And if the player is not past max horizontal speed already
             {
                 // Then apply any acceleration modifiers...
-                if (!_isGrounded) { xSpeedToAdd *= 0.6f; } // (If not on the ground, accelerate less effectively)
+                if (!isGrounded) { xSpeedToAdd *= airControl; } // (If not on the ground, accelerate less effectively)
                 if (xSpeedInInputDirection < 0f) { xSpeedToAdd *= 2f; } // (If turning around, become even more effective at doing so)
 
                 // (If about to hit max speed, accelerate just the right amount)
                 if (xSpeedInInputDirection + xSpeedToAdd > moveSpeed) { xSpeedToAdd = moveSpeed - xSpeedInInputDirection; }
 
                 // And apply player input movement change!
-                _rb.AddForce(xSpeedToAdd / Time.deltaTime * _rb.mass * xInputDirection * Vector2.right);
+                rb.AddForce(xSpeedToAdd / Time.deltaTime * rb.mass * xInputDirection * Vector2.right);
             }
         }
         else // If the player is not trying to move
         {
-            if (_isGrounded && xSpeed != 0f) // And is on the ground and sliding,
+            if (isGrounded && xSpeed != 0f) // And is on the ground and sliding,
             {
                 // Make the acceleration appropriate for skidding to stop... (more effective than accelerating but less effective than turning)
                 xSpeedToAdd *= 1.5f;
@@ -140,8 +149,14 @@ public class PlayerMovement : MonoBehaviour
                 if (xSpeed < xSpeedToAdd) { xSpeedToAdd = xSpeed; }
 
                 // And apply player input movement change!
-                _rb.AddForce(xSpeedToAdd / Time.deltaTime * _rb.mass * -xDirection * Vector2.right);
+                rb.AddForce(xSpeedToAdd / Time.deltaTime * rb.mass * -xDirection * Vector2.right);
             }
+        }
+
+        // Vertical movement
+        if (wallSlidingDirection != 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, 100f));
         }
     }
 
@@ -149,9 +164,16 @@ public class PlayerMovement : MonoBehaviour
 
     void QueueJump(InputAction.CallbackContext context)
     {
-        if (_isGrounded)
+        if (isGrounded)
         {
             Jump();
+        }
+        else if (wallSlidingDirection != 0)
+        {
+            if (rb.velocity.y <= -wallSlideSpeed)
+            {
+                WallJump(wallSlidingDirection);
+            }
         }
         else
         {
@@ -163,14 +185,28 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
         float jumpSpeed = Mathf.Sqrt(-2f * jumpMinHeight * Physics2D.gravity.y);
-        _rb.AddForce(jumpSpeed * _rb.mass * Vector2.up, ForceMode2D.Impulse);
+        rb.AddForce(jumpSpeed * rb.mass * Vector2.up, ForceMode2D.Impulse);
         //Debug.Log("Boing!");
 
         _LandEvent -= Jump;
         
-        _rb.gravityScale = 0;
-        float jumpBonusHeight = Mathf.Abs(_rb.velocity.x) / 12f;
+        rb.gravityScale = 0;
+        float jumpBonusHeight = Mathf.Abs(rb.velocity.x) / 12f;
         float extensionTime = (jumpExtraHeight + jumpBonusHeight) / jumpSpeed;
+        //Debug.Log(extensionTime);
+        _jumpExtensionCoroutine = IJumpExtension(extensionTime);
+        StartCoroutine(_jumpExtensionCoroutine);
+    }
+
+    void WallJump(float wallDirection)
+    {
+        float jumpSpeed = Mathf.Sqrt(-2f * jumpMinHeight * Physics2D.gravity.y);
+        rb.AddForce(new Vector2(moveSpeed * -wallDirection * rb.mass, (jumpSpeed - rb.velocity.y) * rb.mass), ForceMode2D.Impulse);
+        //Debug.Log("Wall boing!");
+
+        rb.gravityScale = 0;
+        float extensionTime = jumpExtraHeight / jumpSpeed;
+        extensionTime /= 2;
         //Debug.Log(extensionTime);
         _jumpExtensionCoroutine = IJumpExtension(extensionTime);
         StartCoroutine(_jumpExtensionCoroutine);
@@ -179,7 +215,7 @@ public class PlayerMovement : MonoBehaviour
     IEnumerator IJumpExtension(float extensionTime)
     {
         yield return new WaitForSeconds(extensionTime);
-        _rb.gravityScale = 1;
+        rb.gravityScale = 1;
         //Debug.Log("Jump maxed! Falling!");
     }
 
@@ -191,7 +227,7 @@ public class PlayerMovement : MonoBehaviour
     void CancelJump()
     {
         _LandEvent -= Jump;
-        _rb.gravityScale = 1;
+        rb.gravityScale = 1;
         if (_jumpExtensionCoroutine != null)
         {
             StopCoroutine(_jumpExtensionCoroutine);
@@ -203,19 +239,51 @@ public class PlayerMovement : MonoBehaviour
 
     void StartCrouch(InputAction.CallbackContext context)
     {
-        _isCrouched = true;
+        isCrouched = true;
     }
 
     void CancelCrouch(InputAction.CallbackContext context)
     {
-        _isCrouched = false;
+        isCrouched = false;
     }
 
     //
 
     bool IsGrounded()
     {
-        return Physics2D.OverlapArea(_boxCollider2D.bounds.center + _boxCollider2D.bounds.extents.x * 0.9f * Vector3.right, _boxCollider2D.bounds.center - _boxCollider2D.bounds.extents + _boxCollider2D.bounds.extents.x * 0.1f * Vector3.right, _ground);
+        float sideBuffer = 0.03f;
+        Vector2 startPoint = boxCollider.bounds.min + sideBuffer * boxCollider.bounds.extents.x * Vector3.right;
+        Vector2 endPoint = startPoint + (1 - sideBuffer) * boxCollider.size.x * Vector2.right;
+        Debug.DrawLine(startPoint, endPoint, Color.yellow);
+        return Physics2D.OverlapArea(startPoint, endPoint, _ground);
+    }
+
+    int WallSliding(float inputDirection) // Returns -1f if pressing into wall on left, 1f if pressing into wall on right, 0f if not pressing against wall
+    {
+        if (isGrounded || inputDirection == 0f)
+        {
+            return 0;
+        }
+        if (inputDirection < 0f)
+        {
+            float sideBuffer = 0.03f;
+            Vector2 startPoint = boxCollider.bounds.min + sideBuffer * boxCollider.bounds.extents.y * Vector3.up;
+            Vector2 endPoint = startPoint + (1 - sideBuffer) * boxCollider.size.y * Vector2.up;
+            Debug.DrawLine(startPoint, endPoint, Color.red);
+            return Physics2D.OverlapArea(startPoint, endPoint, _ground) ? -1 : 0;
+        }
+        else if (inputDirection > 0f)
+        {
+            float sideBuffer = 0.03f;
+            Vector2 startPoint = boxCollider.bounds.max - sideBuffer * boxCollider.bounds.extents.y * Vector3.up;
+            Vector2 endPoint = startPoint - (1 - sideBuffer) * boxCollider.size.y * Vector2.up;
+            Debug.DrawLine(startPoint, endPoint, Color.magenta);
+            return Physics2D.OverlapArea(startPoint, endPoint, _ground) ? 1 : 0;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
     #endregion
@@ -231,12 +299,12 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
 
-        _animator.SetBool("Landing", !_wasGrounded && _isGrounded);
+        _animator.SetBool("Landing", !wasGrounded && isGrounded);
 
-        _animator.SetFloat("RelativeSpeed", Mathf.Abs(_rb.velocity.x) / moveSpeed);
-        _animator.SetFloat("VerticalSpeed", 1f / (1f + Mathf.Exp(_rb.velocity.y / 8f)));
-        _animator.SetBool("Crouching", _isCrouched);
-        _animator.SetBool("Airborne", !_isGrounded);
+        _animator.SetFloat("RelativeSpeed", Mathf.Abs(rb.velocity.x) / moveSpeed);
+        _animator.SetFloat("VerticalSpeed", 1f / (1f + Mathf.Exp(rb.velocity.y / 8f)));
+        _animator.SetBool("Crouching", isCrouched);
+        _animator.SetBool("Airborne", !isGrounded);
     }
 
     #endregion
@@ -255,6 +323,11 @@ public class PlayerMovement : MonoBehaviour
         {
             Time.timeScale = 1f;
         }
+    }
+
+    private void Restart(InputAction.CallbackContext context)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     #endregion
